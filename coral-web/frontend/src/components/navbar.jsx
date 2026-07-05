3b416aeeff9958604f0bf8a516b2da59bcf42e51
lrsmtrrhmy <risma100423@gmail.com>
2026-07-05 15:49:51 +0700

    Update navbar.jsx

--- coral-web/frontend/src/components/navbar.jsx
+++ coral-web/frontend/src/components/navbar.jsx
@@ -1,5 +1,6 @@
 import { Link, useLocation } from 'react-router-dom'
 import { motion } from 'framer-motion'
+import { useState } from 'react'
 import './navbar.css'
 
 const Navbar = ({ user, logout, dark = false }) => {
